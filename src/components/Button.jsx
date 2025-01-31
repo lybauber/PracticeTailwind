e80c8fbc697b53fb9ab132b0f35f1eaac4300031
Brandon <lybauber@gmail>
2025-01-30 20:11:12 -0500

    creando customers y reviews section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 
 
-const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullwidth}) => {
   return (
    <button className={
     `flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
    ${
     backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red rounded-full text-white border-coral-red"
-  }rounded-full w-full"}`}>
+  }rounded-full ${fullwidth && 'w-full'}"}`}>
    
    
         {label}
